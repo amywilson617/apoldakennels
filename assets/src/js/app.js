2aa8a8a6a3e3962e6d0a51214ba998c866bc8f73
Amy Wilson <amy.liz@me.com>
2017-09-30 19:39:13 -0700

    working on fit vid

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,102 +1,13 @@
-//= include ../../../bower_components/jquery/dist/jquery.js
-//= include ../../../bower_components/jquery.fitvids/jquery.fitvids.js
-
-jQuery ( function($) {
-
-	var site = new SiteController($);
-	site.init();
-
-});
-
-function SiteController($)
-{
-	self.init = function()
-	{
-		initFitVid();
-	}
-
-	// MAKES VIDEOS RESIZE RESPONSIVELY
-	function initFitVid() {
-
-		// TARGET YOUTUBE
-		$('iframe[src*="youtube"]').parent().fitVids();
-
-		// TARGET VIMEO
-		$('iframe[src*="vimeo"]').parent().fitVids();
-
-	}
-
-	return self;
-}
+//@prepros-prepend vendor/jquery-3.2.1.js
+//@prepros-prepend vendor/jquery.fitvids.js
 
 
- // CONTACT
 
- // Auto resize input
-function resizeInput() {
-    $(this).attr('size', $(this).val().length);
-}
+$(document).ready(function(){
 
-$('input[type="text"], input[type="email"]')
-    // event handler
-    .keyup(resizeInput)
-    // resize on page load
-    .each(resizeInput);
+      $('iframe[src*="youtube"]').parent().fitVids();
 
 
-console.clear();
-// Adapted from georgepapadakis.me/demo/expanding-textarea.html
-(function(){
 
-  var textareas = document.querySelectorAll('.expanding'),
 
-      resize = function(t) {
-        t.style.height = 'auto';
-        t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
-        t.style.height = (t.scrollHeight + t.offset ) + 'px';
-        t.style.overflow = '';
-      },
-
-      attachResize = function(t) {
-        if ( t ) {
-          console.log('t.className',t.className);
-          t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
-
-          resize(t);
-
-          if ( t.addEventListener ) {
-            t.addEventListener('input', function() { resize(t); });
-            t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
-          }
-
-          t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
-        }
-      };
-
-  // IE7 support
-  if ( !document.querySelectorAll ) {
-
-    function getElementsByClass(searchClass,node,tag) {
-      var classElements = new Array();
-      node = node || document;
-      tag = tag || '*';
-      var els = node.getElementsByTagName(tag);
-      var elsLen = els.length;
-      var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
-      for (i = 0, j = 0; i < elsLen; i++) {
-        if ( pattern.test(els[i].className) ) {
-          classElements[j] = els[i];
-          j++;
-        }
-      }
-      return classElements;
-    }
-
-    textareas = getElementsByClass('expanding');
-  }
-
-  for (var i = 0; i < textareas.length; i++ ) {
-    attachResize(textareas[i]);
-  }
-
-})();
+  });
